1ffa89023ad858c8ea0e8d0949e5ea77528ee034
bttf <adnanchowdhury88@gmail.com>
2015-09-12 20:37:56 -0700

    api urls

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
+import ENV from 'sock/config/environment';
 
 export default DS.RESTAdapter.extend({
-  host: 'http://localhost:3000'
+  host: ENV.footAPI
 });
--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.footAPI = 'http://sock.88mph.io';
   }
 
   return ENV;
